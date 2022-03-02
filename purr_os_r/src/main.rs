9cd232560bf41022f213114cf77552c5858b2389
Lioncat2002 <sankhayan2002@gmail.com>
2022-03-02 17:41:32 +0530

    started working on tests

--- purr_os_r/src/main.rs
+++ purr_os_r/src/main.rs
@@ -1,8 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main="test_main"]
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+
+//Tests
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]){
+    //Runs all the tests which are headed by #[test_case]
+    println!("Running {} tests",tests.len());
+    for test in tests{
+        test();
+    }
+
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("Trivial assertion... ");
+    assert_eq!(1,1);
+    println!("[ok]");
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -13,5 +34,8 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hellow UwU world ");
+    
+    #[cfg(test)]//only runs if `cargo test` command is used
+    test_main(); 
     loop {}
 }
