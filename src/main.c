aea5ceb5ffcb857efae409819003967ce4800460
Lioncat2002 <sankhayan2002@gmail.com>
2022-12-15 21:20:48 +0530

    feat: started working on libc impl

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "monitor.h"
 int main(struct multiboot *mboot_ptr){
     monitor_clear();
-    monitor_write("PurrOS Deadalus\n");
+    monitor_write("PurrOS Muchkin\n");
     monitor_write("version: 0.2\n");
-    return 0xDEADBABA;
+    return 0xCAFEBABE;
 }
