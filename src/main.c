1225532fa2dda03a7a21e9cb113b81856a7cc9d7
Lioncat2002 <sankhayan2002@gmail.com>
2022-12-15 21:41:56 +0530

    feat: working ctrcmp impl

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 
 #include "monitor.h"
+#include "string.h"
 int main(struct multiboot *mboot_ptr){
     monitor_clear();
-    monitor_write("PurrOS Muchkin\n");
+    monitor_write("PurrOS lucipurr\n");
     monitor_write("version: 0.2\n");
+
+    if(strcmp("bruh","bruh")==0){
+        monitor_write("bruh strcmp works!\n");
+    }
+
     return 0xCAFEBABE;
 }
---
+++ src/string.c
@@ -0,0 +1,24 @@
+int strlen(char *s){
+    int i = 0;
+    while(*s++)
+        i++;
+    return i;
+}
+
+int strcmp(const char *dst,char *src){
+
+    int i = 0;
+    while((dst[i]==src[i])){
+        if(src[i++]==0)
+            return 0;
+    }
+    return 1;
+}
+
+int strcpy(char *dst,const char *src){
+    int i = 0;
+    while((dst[i]=src[i++]))
+        ;
+    return i;
+}
+
